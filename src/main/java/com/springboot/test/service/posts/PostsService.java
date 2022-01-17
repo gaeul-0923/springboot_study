6e6740e916169ec878d297d72e96541fd2928574
holic4570 <mvsod@naver.com>
2022-01-17 19:53:50 +0900

    수정처리중

---
+++ src/main/java/com/springboot/test/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package com.springboot.test.service.posts;
+
+import com.springboot.test.domain.posts.Posts;
+import com.springboot.test.domain.posts.PostsRepository;
+import com.springboot.test.web.dto.PostsResponseDto;
+import com.springboot.test.web.dto.PostsSaveRequestDto;
+import com.springboot.test.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
--- src/test/java/com/springboot/test/TestApplicationTests.java
+++ src/test/java/com/springboot/test/TestApplicationTests.java
@@ -1,6 +1,6 @@
 package com.springboot.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
